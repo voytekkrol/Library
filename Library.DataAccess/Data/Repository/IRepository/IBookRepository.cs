9a0b2b30a638d2de1a2e505bbc5bb5b81781b1f5
voytekkrol <wojtekmkrol@gmail.com>
2021-02-06 13:25:07 +0100

    Implement Repository/UnitOfWork  pattern

---
+++ Library.DataAccess/Data/Repository/IRepository/IBookRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.DataAccess.Data.Repository.IRepository
+{
+    interface IBookRepository
+    {
+    }
+}
---
+++ Library.Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Library.Models
+{
+    class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public DateTime PublishedDate { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
