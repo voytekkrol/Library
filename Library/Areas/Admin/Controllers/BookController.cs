d2c3a652b59153113faa68c983ea42c397d1e037
voytekkrol <wojtekmkrol@gmail.com>
2021-02-08 11:56:07 +0100

    Repair book.js, and Upsert View

--- Library/Areas/Admin/Controllers/BookController.cs
+++ Library/Areas/Admin/Controllers/BookController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Library.DataAccess.Data.Repository.IRepository;
+using Library.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library.Areas.Admin.Controllers
@@ -21,5 +22,69 @@ namespace Library.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public IActionResult Upsert(int? id)
+        {
+            Book book= new Book();
+            if (id == null)
+            {
+                return View(book);
+            }
+
+            book= _unitOfWork.Book.Get(id.GetValueOrDefault());
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                if (book.Id == 0)
+                {
+                    _unitOfWork.Book.Add(book);
+                }
+                else
+                {
+                    _unitOfWork.Book.Update(book);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitOfWork.Book.GetAll() });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Book.Get(id);
+
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting." });
+            }
+
+            _unitOfWork.Book.Remove(objFromDb);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete successful" });
+        }
+
+        #endregion
     }
 }
---
+++ Library/wwwroot/js/book.js
@@ -0,0 +1,40 @@
+﻿
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/admin/book/GetAll",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "40%" },
+            { "data": "author", "width": "30%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a href="/Admin/book/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;'>
+                                    <i class='far fa-edit'></i> Edit
+                                </a>
+                                &nbsp;
+                                <a onclick=Delete("/Admin/book/Delete/${data}") class='btn btn-danger text-white' style='cursor:pointer; width:100px;'>
+                                    <i class='far fa-trash-alt'></i> Delete
+                                </a>
+                            </div>
+                            `;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No records found."
+        },
+        "width": "100%"
+    });
+}
